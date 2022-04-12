489422f62be825ed5330814c866530653274405b
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-04-12 08:12:55 +0300

    Rework player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 8f;
+    [Header("Movement")]
+    [SerializeField] private float _movementSpeed = 8f;
 
-    private float horizontalInput;
-    private float verticalInput;
+    [Header("Rotation")]
+    [SerializeField] private float _rotationSmoothTime = 0.1f;
+    [SerializeField] private float _rotationSpeed = 50f;
 
-    private Rigidbody rb;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private Camera mainCamera;
+
+    private Rigidbody _rb;
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+            Debug.LogError($"Failed to get rigidbody in {gameObject.name}");
+
+        mainCamera = Camera.main;
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput)
-            * movementSpeed;
+        if(_horizontalInput == 0 && _verticalInput == 0)
+            return;
 
-        rb.velocity = movement;
+        Vector3 movement = new Vector3(_horizontalInput, 0f, _verticalInput)
+            * _movementSpeed
+            * Time.fixedDeltaTime;
+
+        float targetAngle = Mathf.Atan2(movement.x, movement.z)
+            * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+        float refRotationSmoothVelocity = _rotationSmoothTime;
+        float angle = Mathf.SmoothDampAngle(
+            transform.eulerAngles.y,
+            targetAngle,
+            ref refRotationSmoothVelocity,
+            _rotationSmoothTime
+        );
+
+        Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+        _rb.MovePosition(_rb.position + moveDirection.normalized * _movementSpeed * Time.fixedDeltaTime);
+        _rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
     }
 }
