64f55ec44a5889f8def8d19fff663b34d5157e10
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-06-17 00:11:25 +0300

    DeathCollider.cs
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCollider : MonoBehaviour
+{
+    [SerializeField] private int _instantDeathDamage = 99999;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health health = collision.transform.GetComponent<Health>();
+        if(health == null)
+            return;
+
+        health.TakeDamage(_instantDeathDamage);
+    }
+}
