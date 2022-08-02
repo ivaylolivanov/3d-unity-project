8574c4c34e88489c2c9f7ccdb3db889ecac33f69
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-08-02 22:43:42 +0300

    Navigation mesh experiments part 2
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Terrain/NavigationMesh/PolygonGenerator.cs
+++ Assets/Scripts/Terrain/NavigationMesh/PolygonGenerator.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PolygonGenerator : MonoBehaviour
 {
     [SerializeField] Transform[] _terrainObjects;
-    // [SerializeField] float _diviationAngleThreshold = 30f;
+    [SerializeField] float _diviationAngleThreshold = 30f;
 
-    Color[] colors = new Color[] {
+    private Color[] colors = new Color[] {
         Color.red,
         Color.blue,
         Color.green,
@@ -18,9 +18,16 @@ public class PolygonGenerator : MonoBehaviour
         Color.white
     };
 
+    private HashSet<Vector3> polygonPoints;
+
+    void OnEnable()
+    {
+        polygonPoints = new HashSet<Vector3>();
+    }
+
     void OnDrawGizmos()
     {
-        // Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         foreach (var terrain in _terrainObjects)
         {
             Mesh terrainMesh = terrain.GetComponent<MeshFilter>()?.sharedMesh;
@@ -34,41 +41,51 @@ public class PolygonGenerator : MonoBehaviour
             int triangleIndex = 0;
             while((triangleIndex * 3) < triangles.Length)
             {
-                Debug.Log($"Indices: {triangles[triangleIndex * 3]}, {triangles[triangleIndex * 3 + 1]}, {triangles[triangleIndex * 3 + 2]}");
-                ++triangleIndex;
-
                 if((triangleIndex * 3 + 2) >= triangles.Length)
                     break;
 
-                // Gizmos.color = colors[colorIndex];
                 Vector3 vertex1 = vertices[triangles[triangleIndex * 3]];
                 Vector3 vertex2 = vertices[triangles[triangleIndex * 3 + 1]];
                 Vector3 vertex3 = vertices[triangles[triangleIndex * 3 + 2]];
 
-                Gizmos.DrawLine(vertex1, vertex2);
-                Gizmos.DrawLine(vertex1, vertex3);
-                Gizmos.DrawLine(vertex2, vertex3);
+                Vector3 a = vertex2 - vertex1;
+                Vector3 b = vertex3 - vertex1;
+
+                Vector3 triangleNormal = new Vector3(
+                    a.y * b.z - a.z * b.y,
+                    a.z * b.x - a.x * b.z,
+                    a.x * b.y - a.y * b.x
+                );
+
+                float diviationAngle = Vector3.Angle(Vector3.up, triangleNormal);
+                if (diviationAngle > _diviationAngleThreshold)
+                    continue;
+
+                polygonPoints.Add(Vector3.Scale(vertex1, terrain.localScale));
+                polygonPoints.Add(Vector3.Scale(vertex2, terrain.localScale));
+                polygonPoints.Add(Vector3.Scale(vertex3, terrain.localScale));
+
+                Gizmos.DrawRay(terrain.position, triangleNormal * 10);
+
+                ++triangleIndex;
             }
 
-            // foreach (var normala in normals)
+            // for (int i = 0; i < vertices.Length; ++i)
             // {
-            //     var diviationAngle = Vector3.Angle(Vector3.up, normala);
-            //     if (diviationAngle > _diviationAngleThreshold)
+            //     var scaledVertex = Vector3.Scale(vertices[i], terrain.localScale);
+
+            //     if (scaledVertex.y <= 0)
             //         continue;
 
-            //     Gizmos.DrawRay(terrain.position, normala * 10);
-
-            for (int i = 0; i < vertices.Length; ++i)
-                {
-                    var scaledVertex = Vector3.Scale(vertices[i], terrain.localScale);
-
-                    if (scaledVertex.y <= 0)
-                        continue;
-
-                    var center = Vector3.Scale(vertices[i], terrain.localScale);
-                    Gizmos.DrawSphere(center, .2f);
-                }
+            //     var center = Vector3.Scale(vertices[i], terrain.localScale);
+            //     Gizmos.DrawSphere(center, .2f);
             // }
+        }
+
+        Gizmos.color = Color.red;
+        foreach(var point in polygonPoints)
+        {
+            Gizmos.DrawSphere(point, .2f);
         }
     }
 }
