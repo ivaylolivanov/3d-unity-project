f8aef2657be7be932ba218e6ec9bc9261f97349f
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-09-07 19:46:12 +0300

    Unit: Responsible for taking damage and healing
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -6,13 +6,54 @@ public class Unit : MonoBehaviour
 
     public UnitData UnitData => _unitData;
 
-    protected Health _health;
+    protected HealthUI _healthUI;
+
+    protected int _health = 0;
+    protected int _maxHealth = 0;
 
     protected virtual void OnEnable()
     {
-        _health = GetComponentInChildren<Health>();
+        _health    = _unitData.InitialHealth;
+        _maxHealth = _unitData.InitialHealth;
+
+        _healthUI = GetComponentInChildren<HealthUI>();
+
+        if (_healthUI == null)
+        {
+            Debug.LogError($"Failed to get {_healthUI.GetType()} in {gameObject.name}");
+        }
+        else
+        {
+            _healthUI.UpdateCurrentValue(_health);
+            _healthUI.UpdateMaxValue(_maxHealth);
+        }
     }
 
-    public void TakeDamage(int damage) => _health.TakeDamage(damage);
-    public void Heal(int amount)       => _health.Heal(amount);
+    public void TakeDamage(int damage)
+    {
+        if(damage <= 0)
+            return;
+
+        _health -= damage;
+
+        _healthUI.UpdateCurrentValue(_health);
+
+        if(_health <= 0) Die();
+    }
+
+    public void Heal(int amount)
+    {
+        if(amount <= 0)
+            return;
+
+        _health += amount;
+        _health = Mathf.Clamp(_health, 0, _maxHealth);
+
+        _healthUI.UpdateCurrentValue(_health);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
 }
