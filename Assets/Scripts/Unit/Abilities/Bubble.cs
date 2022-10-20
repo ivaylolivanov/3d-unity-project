51623259653b915e2120d724c3f8c4cdbc8a1a5d
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-10-20 23:47:54 +0300

    Bubble: Add OnTriggerExited
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Unit/Abilities/Bubble.cs
+++ Assets/Scripts/Unit/Abilities/Bubble.cs
@@ -4,7 +4,11 @@ using UnityEngine.Events;
 public class Bubble : MonoBehaviour
 {
     public UnityAction<GameObject, GameObject> OnTriggerEntered;
+    public UnityAction<GameObject, GameObject> OnTriggerExited;
 
     private void OnTriggerEnter(Collider other)
         => OnTriggerEntered?.Invoke(other.gameObject, gameObject);
+
+    private void OnTriggerExit(Collider other)
+        => OnTriggerExited?.Invoke(other.gameObject, gameObject);
 }
